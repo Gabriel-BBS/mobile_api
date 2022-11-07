fc12c6145d0f65a85405664c43b42b8aa8c873d1
gabriel <gabriel_silva90@hotmail.com>
2022-11-07 20:16:08 -0300

    codigo rodando

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// const path = require('path');
-// const cors = require("cors")
+const path = require('path');
 const http = require("http");
 const express = require('express');
+// const { Router } = require('express');
 // const handlebars = require('express-handlebars');
 const app = express();
 // const urlencodeParser = bodyParser.urlencoded({extended:false});
@@ -9,11 +9,12 @@ const app = express();
 
 //teste
 app.get("/",function(req,res){
-    res.send("index.html");
+    res.sendFile('views/index.html', {root: __dirname })
 });
 
 // app.use('/js',express.static('js'));
-// app.use('/img',express.static('img'));
+app.use('/css',express.static('css'));
+app.use('/views',express.static('views'));
 
 //Start server
 http.createServer(app).listen(3000,()=>console.log("Servidor está rodando!"));
