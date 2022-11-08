d23b73b79527b03702f156301b3aa5cd804e7843
gabriel <gabriel_silva90@hotmail.com>
2022-11-07 23:18:17 -0300

    consumo api - done

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const path = require('path');
 const http = require("http");
 const express = require('express');
-// const { Router } = require('express');
-// const handlebars = require('express-handlebars');
 const app = express();
-// const urlencodeParser = bodyParser.urlencoded({extended:false});
 
 
-//teste
+//routes
 app.get("/",function(req,res){
     res.sendFile('views/index.html', {root: __dirname })
 });
 
-// app.use('/js',express.static('js'));
 app.use('/css',express.static('css'));
 app.use('/views',express.static('views'));
-
 //Start server
 http.createServer(app).listen(3000,()=>console.log("Servidor está rodando!"));
